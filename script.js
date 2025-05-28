5d60f6b03b4e515f8235286e283f89cf0581d770
Rishav-R03 <rishav042023@gmail.com>
2025-05-29 00:07:13 +0530

    change in dir

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thank you for reaching out. I will get back to you soon!');
-  this.reset();
-});
